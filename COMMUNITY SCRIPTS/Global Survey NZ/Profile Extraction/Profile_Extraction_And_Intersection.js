ec135c8f4a4f75980a28644f0a5b7a5f7bec0f16
Thomas Mathey <Thomas@SURVEY.local>
2026-07-09 14:18:16 +1200

    Fix SCloud.FromClick returning an object instead of cloud

--- COMMUNITY SCRIPTS/Global Survey NZ/Profile Extraction/Profile_Extraction_And_Intersection.js
+++ COMMUNITY SCRIPTS/Global Survey NZ/Profile Extraction/Profile_Extraction_And_Intersection.js
@@ -23,8 +23,10 @@ function main() {
     } else if (allClouds.length > 1) {
         // Multiple clouds exist: ask the user to click the one they want
         print("Multiple clouds detected. Please CLICK on the cloud you want to use...");
-        selCloud = SCloud.FromClick();
-    }
+        var clickRes = SCloud.FromClick();
+        if (clickRes && clickRes.ErrorCode === 0) {
+            selCloud = clickRes.Cloud;
+        }
     
     if (!selCloud) {
         throw new Error("No point cloud found. Please ensure a point cloud is loaded.");
